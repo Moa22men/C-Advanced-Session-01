9b31545d46cd26d18f548a1050fb03be2fab1038
Moa22men <moamenabdelrahman22@gmail.com>
2025-07-26 12:13:05 +0300

    Part 1 solved

--- C# Advanced Session 01/Program.cs
+++ C# Advanced Session 01/Program.cs
@@ -4,9 +4,36 @@ namespace C__Advanced_Session_01
 {
     internal class Program
     {
-        static void Main(string[] args)
+        #region Part 1
+        static void OptimizedBubbleSort(int[] array)
         {
-            Console.WriteLine("Hello World!");
+            int n = array.Length;
+            bool flag;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                flag = false;
+
+                for (int j = 0; j < n - 1 - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+
+                        flag = true;
+                    }
+                }
+
+                if (!flag)
+                    break;
+            }
+            #endregion
+            static void Main(string[] args)
+            {
+                
+            }
         }
     }
 }
