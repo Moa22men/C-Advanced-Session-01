1f8334f3395f844be2013c4fb1257b488ce116b6
Moa22men <moamenabdelrahman22@gmail.com>
2025-07-26 12:15:03 +0300

    Part 2 solved

--- C# Advanced Session 01/Program.cs
+++ C# Advanced Session 01/Program.cs
@@ -2,6 +2,33 @@
 
 namespace C__Advanced_Session_01
 {
+    #region Part 2
+    public class Range<T> where T : IComparable<T>
+    {
+        private T min;
+        private T max;
+
+        public Range(T minimum, T maximum)
+        {
+            if (minimum.CompareTo(maximum) > 0)
+                Console.WriteLine("Minimum should not be greater than Maximum.");
+
+            min = minimum;
+            max = maximum;
+        }
+
+        public bool IsInRange(T value)
+        {
+            return value.CompareTo(min) >= 0 && value.CompareTo(max) <= 0;
+        }
+
+        public dynamic Length()
+        {
+            return (dynamic)max - (dynamic)min;
+        }
+    }
+
+    #endregion
     internal class Program
     {
         #region Part 1
